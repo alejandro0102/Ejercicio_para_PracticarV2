1245e51d7bba05e175549dc6298c539c40d0c8e2
Alejandro De La Torre <insaciablevyy@gmail.com>
2023-05-21 10:38:34 +0200

    El mejor commit

---
+++ src/Repositorios/Practicar.java
@@ -0,0 +1,10 @@
+package Repositorios;
+
+public class Practicar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola me esta ayudando Antonio");
+	}
+
+}
