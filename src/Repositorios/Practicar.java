88e9e65179c11f665b56ba9c0666ebb4bcec8a26
Alejandro De La Torre <insaciablevyy@gmail.com>
2023-05-21 10:57:36 +0200

    He añadido Hola Mundo

--- src/Repositorios/Practicar.java
+++ src/Repositorios/Practicar.java
@@ -1,10 +1,13 @@
 package Repositorios;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class Practicar {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola me esta ayudando Antonio");
+		System.out.println("Hola Mundo");
 	}
 
 }
